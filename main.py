2e85eb68e981e3aa1e358c066f6f285012cd19d6
foxpilot64 <jedioutcastbex@gmail.com>
2024-09-16 16:49:24 -0400

    Added char counts to main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+def main():
+    with open("/home/bex/workspace/github.com/foxpilot64/bookbot/books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+        words = file_contents.split()
+        print (len(words))
+
+        #call charnum here, inside of main:
+        result = charnum(file_contents)
+        print("Character counts: ")
+        print(result)
+
+def charnum(text):
+    lowered_text = text.lower()
+    char_counts = {} #Store counts in a dictionary
+   
+
+    #Count each char in lowered_text:
+    for char in lowered_text:
+        if char.isalpha():
+            if char in char_counts:
+                char_counts[char] += 1
+            else:
+                char_counts[char] = 1
+    
+    
+    return char_counts
+    
+  
+
+main()
