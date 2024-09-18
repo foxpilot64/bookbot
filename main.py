e7bef2eeabfffad09e17eace340dbc78b68bd4c5
foxpilot64 <jedioutcastbex@gmail.com>
2024-09-18 14:07:20 -0400

    Created a sorted list of dictionaries to print the character count results.

--- main.py
+++ main.py
@@ -4,12 +4,26 @@ def main():
         print(file_contents)
 
         words = file_contents.split()
-        print (len(words))
+        print (f"{len(words)} words found in the document\n")
 
         #call charnum here, inside of main:
         result = charnum(file_contents)
         print("Character counts: ")
         print(result)
+        #Convert the dictionary to a list of dictionaries using sorting
+        alpha_count = [{"character": char, "num": count} for char, count in result.items()]
+
+        #Sort the list of dictionaries by the "num" value in descending order:
+        alpha_count.sort(reverse=True, key=lambda d: d["num"])
+
+        #Print sorted character counts with formatting
+        for entry in alpha_count:
+            character = entry["character"]
+            num = entry["num"]
+            print(f"The '{character}' character was found {num} times")
+
+
+
 
 def charnum(text):
     lowered_text = text.lower()
@@ -27,7 +41,27 @@ def charnum(text):
     
     return char_counts
     
+  
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
    # list_of_dict(): []
+#def sort_on(dict):
+   # return dict["num"]
+
+##alpha_count = [
+
+#]
+#alpha_count.sort(reverse=True, key=sort_on)
+#print(alpha_count)
 
 
 main()
