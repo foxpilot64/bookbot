19d0bd0c4d35c5254da328473fe0a9263370e180
foxpilot64 <jedioutcastbex@gmail.com>
2024-09-18 14:45:22 -0400

    made organized comments to explain each section.

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 def main():
     with open("/home/bex/workspace/github.com/foxpilot64/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
+
+        #Print only the first 1000 characters to preview content instead of all
         print(file_contents)
 
+        #Split the content into words and count them
         words = file_contents.split()
-        print (f"{len(words)} words found in the document\n")
+        print (f"\n{len(words)} words found in the document\n")
 
         #call charnum here, inside of main:
         result = charnum(file_contents)
-        print("Character counts: ")
-        print(result)
+        
+        
         #Convert the dictionary to a list of dictionaries using sorting
         alpha_count = [{"character": char, "num": count} for char, count in result.items()]
 
         #Sort the list of dictionaries by the "num" value in descending order:
         alpha_count.sort(reverse=True, key=lambda d: d["num"])
-
+        
+        #The beginning of sorted report:
+        print("\n---Begin report of sorted character counting---\n")
+        
         #Print sorted character counts with formatting
         for entry in alpha_count:
             character = entry["character"]
             num = entry["num"]
             print(f"The '{character}' character was found {num} times")
+
+    print("\n--- End report ---")
 
 
 
